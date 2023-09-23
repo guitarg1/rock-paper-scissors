d7ff24e569b3708b1fd8971d385eeeaf528686fb
Mike <strain.m@gmail.com>
2023-09-23 08:54:02 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ order to work correctly.
   
   
 
-
+/*
 
 let userWins = 0;
     let computerWins = 0;
@@ -65,10 +65,80 @@ let userWins = 0;
     }
     
     playGame();
+*/
 
-
-
-
-
-
-
+// does this work
+let playerScore = 0;
+        let computerScore = 0;
+        const winningScore = 5;
+        
+        function computerPlay() {
+            const choices = ['rock', 'paper', 'scissors'];
+            const randomIndex = Math.floor(Math.random() * choices.length);
+            return choices[randomIndex];
+        }
+        
+        function playRound(playerSelection, computerSelection) {
+            if (playerSelection === computerSelection) {
+                return 'It\'s a tie!';
+            } else if (
+                (playerSelection === 'rock' && computerSelection === 'scissors') ||
+                (playerSelection === 'paper' && computerSelection === 'rock') ||
+                (playerSelection === 'scissors' && computerSelection === 'paper')
+            ) {
+                playerScore++;
+                return 'You win this round!';
+            } else {
+                computerScore++;
+                return 'Computer wins this round!';
+            }
+        }
+        
+        function updateScore() {
+            document.getElementById('player-score').textContent = playerScore;
+            document.getElementById('computer-score').textContent = computerScore;
+        }
+        
+        function checkWinner() {
+            if (playerScore === winningScore) {
+                document.getElementById('round-result').textContent = 'Congratulations! You win the game!';
+                disableButtons();
+            } else if (computerScore === winningScore) {
+                document.getElementById('round-result').textContent = 'Computer wins the game!';
+                disableButtons();
+            }
+        }
+        
+        function disableButtons() {
+            document.getElementById('rock').disabled = true;
+            document.getElementById('paper').disabled = true;
+            document.getElementById('scissors').disabled = true;
+            document.getElementById('play-again').style.display = 'block';
+        }
+        
+        function enableButtons() {
+            document.getElementById('rock').disabled = false;
+            document.getElementById('paper').disabled = false;
+            document.getElementById('scissors').disabled = false;
+            document.getElementById('play-again').style.display = 'none';
+        }
+        
+        function playGame(playerSelection) {
+            const computerSelection = computerPlay();
+            const roundResult = playRound(playerSelection, computerSelection);
+            document.getElementById('round-result').textContent = roundResult;
+            updateScore();
+            checkWinner();
+        }
+        
+        document.getElementById('rock').addEventListener('click', () => playGame('rock'));
+        document.getElementById('paper').addEventListener('click', () => playGame('paper'));
+        document.getElementById('scissors').addEventListener('click', () => playGame('scissors'));
+        
+        document.getElementById('play-again').addEventListener('click', () => {
+            playerScore = 0;
+            computerScore = 0;
+            updateScore();
+            enableButtons();
+            document.getElementById('round-result').textContent = 'Choose an option:';
+        });
